77c6d9bbe446efa11c209194572f1f5092d40a0b
Oliver S. Larsen <Olle_s_larsen@hotmail.com>
2015-11-24 15:53:16 +0100

    added DiceCup and Dice

--- 39_CDIO3/src/entity/Dice.java
+++ 39_CDIO3/src/entity/Dice.java
@@ -2,4 +2,25 @@ package entity;
 
 public class Dice {
 
+private int DiceEyes;
+private int DiceSides;
+	
+public Dice(){
+	DiceSides= 6;
+	
+}
+
+public int rollDie(){
+	DiceEyes= (int) (Math.random()*DiceSides+1);
+	return DiceEyes;
+	
+}
+
+
+public int getDiceEyes(){
+	return DiceEyes;
+}
+
+
+
 }

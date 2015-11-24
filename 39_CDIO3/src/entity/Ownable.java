c31b3094db21092e0b25b154cb79f3170e0a1fa0
frederik <frederik@10.16.99.60>
2015-11-24 16:07:25 +0100

    tilføjet ownable body

--- 39_CDIO3/src/entity/Ownable.java
+++ 39_CDIO3/src/entity/Ownable.java
@@ -1,5 +1,42 @@
 package entity;
 
-public class Ownable {
+import java.awt.Color;
+
+public abstract class Ownable extends Field {
+
+	private int price;
+	private Player owner;
+	private boolean isOwned;
+
+	//konstruktor
+	public Ownable( String fieldName, int price, Color foregroundColor, Color backColor, String description, Player owner, boolean isOwned) {
+		super(fieldName, foregroundColor, backColor, description);
+		this.price = price;
+		this.owner = owner;
+		this.isOwned = false;
+	}
+
+	public abstract int getRent();
+	//public abstract int getRent(DiceCup dc);
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Player getOwner () {
+		return owner;
+	}
+	public void setOwner (Player owner) {
+		this.owner = owner;
+	}
+
+	public boolean getOwned() {
+		return isOwned;
+	}
+	public void setOwned(boolean isOwned) {
+		this.isOwned = isOwned;
+	}
 
 }

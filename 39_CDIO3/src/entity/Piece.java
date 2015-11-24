d53b636c56418a09657b870fabc3b484c953fd93
Micha_000 <Micha_000@10.16.162.165>
2015-11-24 15:55:57 +0100

    Hej

--- 39_CDIO3/src/entity/Piece.java
+++ 39_CDIO3/src/entity/Piece.java
@@ -2,7 +2,24 @@ package entity;
 
 public class Piece {
 
-	
-	
+	// Initialiserer attribut
+		private int playerNumber;
+
+		// Konstruktoer giver brikken et spillernummer den hører til
+		public Piece(int playerNumber){
+			this.playerNumber = playerNumber;
+		}
+		
+		public int getPlayerNumber(){
+			
+			// Returner hvilket spillernummer objektet(brik) hoerer til
+			return this.playerNumber;
+		}
+		
+		public String getPlayerName(){
+			
+			// Returner SpillerNavn baseret paa spillernummer
+			return "Player " + playerNumber;
+		}
 	
 }

d9c70b861cae4ff648bf5a76ce59de6069b3c928
Oliver S. Larsen <Olle_s_larsen@hotmail.com>
2015-11-24 16:05:09 +0100

    mere Field

--- 39_CDIO3/src/entity/Field.java
+++ 39_CDIO3/src/entity/Field.java
@@ -1,5 +1,50 @@
 package entity;
 
-public class Field {
+import java.awt.Color;
 
-}
+
+public abstract class Field {
+
+		
+		private String fieldName;
+		private  Color forgroundColor;
+		private  Color backgroundColor;
+		private String description;
+		
+		
+		public Field(String fieldName, Color forgroundColor, Color backgroundColor, String description) {
+			super();
+			this.fieldName = fieldName;
+			this.forgroundColor = forgroundColor;
+			this.backgroundColor = backgroundColor;
+			this.description = description;
+		}
+
+		
+	public abstract void landOnField(Player player);
+
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+
+	public Color getForgroundColor() {
+		return forgroundColor;
+	}
+
+
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+	}
+
+
+	
+

9cdbe9c48f776e9b648bf7437dae00f8bf844dbc
frederik <frederik@10.16.99.60>
2015-11-24 15:53:43 +0100

    added account metoder

--- 39_CDIO3/src/entity/Account.java
+++ 39_CDIO3/src/entity/Account.java
@@ -2,4 +2,27 @@ package entity;
 
 public class Account {
 
+	private int startAmount = 30000;
+	private int balance;
+	
+	Account () {
+		this.balance = startAmount;
+	}
+	
+	public int getBalance () {		
+		return this.balance;
+	}
+	
+	public void setBalance (int balance) {
+		this.balance = balance;
+	}
+
+	public int addBalance ( int change) {
+		return balance += change;
+	}
+
+	public int subBalance ( int change) {
+		return balance -= change;
+	}
+
 }

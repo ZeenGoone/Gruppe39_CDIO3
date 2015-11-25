b9c05cba28cf0a8eac7753b8c1e85b87262b0a83
Oliver S. Larsen <Olle_s_larsen@hotmail.com>
2015-11-25 12:33:09 +0100

    Refuge field class finished

--- 39_CDIO3/src/entity/Refuge.java
+++ 39_CDIO3/src/entity/Refuge.java
@@ -1,5 +1,33 @@
 package entity;
 
-public class Refuge {
+import java.awt.Color;
 
+public class Refuge extends Field {
+private int bonus;
+
+	public Refuge(String fieldName, Color forgroundColor, Color backgroundColor, String description, int bonus) {
+		super(fieldName, forgroundColor, backgroundColor, description);
+		this.bonus = bonus;
+	}
+
+	
+	
+public int getBonus(){
+	return bonus;
+}
+	
+	
+	
+	@Override
+	public void landOnField(Player player) {
+		player.updateBalance(getBonus());
+		System.out.println(player.getPiece().getPlayerName() + "got bonus:" + bonus);
+		
+	}
+
+
+	
+	
+	
+	
 }

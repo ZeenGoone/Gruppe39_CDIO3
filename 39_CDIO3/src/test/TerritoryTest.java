c15971dea029adbe595d501286a2a2b173871ae8
Oliver <Oliver@DESKTOP-O0IVE04.home>
2015-11-26 17:55:52 +0100

    oprettet testpakke og knap startet på territorytest..

---
+++ 39_CDIO3/src/test/TerritoryTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import entity.Player;
+import entity.Territory;
+import entity.Field;
+
+public class TerritoryTest {
+private Player player;
+private Field territory1500;
+
+	
+	
+	
+	@Before
+	public void SetUp() throws Exception {
+		this.player = new Player();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

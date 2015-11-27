17c07397ae0020627ecc66a297e3bc86a197f1e3
zeeng <zeeng@192.168.1.37>
2015-11-27 23:36:28 +0100

    Lavet testsuite

---
+++ 39_CDIO3/src/test_of_fields/FieldTests.java
@@ -0,0 +1,11 @@
+package test_of_fields;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FleetTest.class, LaborCampTest.class, RefugeTest.class, TaxTest.class, TerritoryTest.class })
+public class FieldTests {
+
+}
